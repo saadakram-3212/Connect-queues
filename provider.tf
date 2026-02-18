33219b1b5a9f60e967cbae88bc1a135ae2e23547
saadakram-3212 <saad.akram@eurustechnologies.com>
2026-02-18 21:52:54 +0500

    refactor/provider fixed

--- provider.tf
+++ provider.tf
@@ -4,23 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 1.0.0"
-    }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      CreatedBy = "Terraform"
-    }
-  }
-}
-
-provider "awscc" {
   region = var.aws_region
 }
